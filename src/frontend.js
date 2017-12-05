ab19451de8875974a03281dc8982dcda10102a18
Gregor Middell <gregor@middell.net>
2017-12-05 13:40:41 +0100

    Sketch navbar

--- src/frontend.js
+++ src/frontend.js
@@ -1,2 +1,13 @@
 const $ = require("jquery");
+
+$(".navbar-burger").each(function() {
+    const $this = $(this);
+    const $target = $this.closest(".navbar").find(".navbar-menu");
+
+    $this.on("click", function() {
+        $this.toggleClass("is-active");
+        $target.toggleClass("is-active");
+    });
+});
+
 $("html").removeClass("setting-up");
