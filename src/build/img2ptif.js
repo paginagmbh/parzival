e74cc1ec91139a913523caa5b169bec39176cd9b
Gregor Middell <gregor@middell.net>
2018-11-07 12:15:17 +0100

    Fix porting bug

--- src/build/img2ptif.js
+++ src/build/img2ptif.js
@@ -8,7 +8,7 @@ const imageDir = process.env.PARZIVAL_IMAGE_BASE || '/srv/bern'
 const ptifDir = process.env.PARZIVAL_PTIF_BASE || '/var/www/iiif'
 
 const convert = async () => {
-  for (let base in ['HS V unkomprimiert', 'HS VV unkomprimiert']) {
+  for (let base of ['HS V unkomprimiert', 'HS VV unkomprimiert']) {
     base = resolve(imageDir, base)
     for (const image of (await globby(['VV*.JPG', '*.tif'].map(p => resolve(base, p))))) {
       const ext = extname(image)
