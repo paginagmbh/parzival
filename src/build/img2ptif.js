6e6aeb0c1d7a85a535e4c0290101b7f0e4b182a2
Gregor Middell <gregor@middell.net>
2019-03-11 10:22:56 +0100

    Generate numbered facsimile versions

--- src/build/img2ptif.js
+++ src/build/img2ptif.js
@@ -7,32 +7,47 @@ import xfs from 'fs-extra'
 import { cwd, encoding } from './util'
 
 const imageDir = process.env.PARZIVAL_IMAGE_BASE || '/srv/bern'
-const ptifDir = process.env.PARZIVAL_PTIF_BASE || '/var/www/iiif/neu'
+const ptifDir = process.env.PARZIVAL_PTIF_BASE || '/var/www/iiif'
+
+const bases = []
+for (const numbered of ['unbeziffert', 'beziffert']) {
+  for (const manuscript of ['V', 'VV']) {
+    bases.push(`HS ${manuscript} neu ${numbered}`)
+  }
+}
 
 const convert = async () => {
-  for (let base of ['HS V neu unbeziffert', 'HS VV neu unbeziffert']) {
-    base = resolve(imageDir, base)
-    for (const image of (await globby(['*.tif', '*.TIF'].map(p => resolve(base, p))))) {
-      const ext = extname(image)
-      const imageBase = basename(image, ext).toLowerCase()
+  let processed = -1
+  while (processed !== 0) {
+    processed = 0
+    for (let base of bases) {
+      const numbered = !base.includes('unbeziffert')
+      base = resolve(imageDir, base)
+      const images = await globby(['*.tif', '*.TIF'].map(p => resolve(base, p)))
+      for (const image of images) {
+        const ext = extname(image)
+        const imageBase = basename(image, ext).toLowerCase()
 
-      if (!imageBase.match(/^v{1,2}[0-9]{3}[rv]$/)) continue
+        if (!imageBase.match(/^v{1,2}[0-9]{3}[rv]$/)) continue
 
-      const ptif = resolve(ptifDir, `${imageBase}.ptif`)
-      if (await xfs.pathExists(ptif)) continue
+        const suffix = numbered ? '_num' : ''
+        const ptif = resolve(ptifDir, `${imageBase}${suffix}.ptif`)
+        if (await xfs.pathExists(ptif)) continue
 
-      const result = cp.spawnSync('vips', [
-        'tiffsave',
-        image, ptif,
-        '--tile', '--pyramid',
-        '--compression=jpeg',
-        '--tile-width', '256', '--tile-height', '256'
-      ], { cwd, encoding, stdio: 'inherit' })
+        const result = cp.spawnSync('vips', [
+          'tiffsave',
+          image, ptif,
+          '--tile', '--pyramid',
+          '--compression=jpeg',
+          '--tile-width', '256', '--tile-height', '256'
+        ], { cwd, encoding, stdio: 'inherit' })
 
-      if (result.status !== 0) {
-        await xfs.remove(ptif)
-      } else {
-        console.log(ptif)
+        if (result.status !== 0) {
+          await xfs.remove(ptif)
+        } else {
+          console.log(ptif)
+        }
+        processed += 1
       }
     }
   }
