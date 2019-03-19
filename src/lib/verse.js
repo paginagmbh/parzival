753715c948fbf02d18b9ec9383fcf41a5971c674
Gregor Middell <gregor@middell.net>
2019-03-19 12:36:28 +0100

    Enhance support for paratextual insertions via [0...] subscripts

--- src/lib/verse.js
+++ src/lib/verse.js
@@ -33,26 +33,24 @@ export const parse = (str) => {
   if (!nums.match(/^[0-9.]+/g)) throw new Error(nums)
   nums = nums.split('.').map(n => parseInt(n, 10))
 
-  const isHeading = (nums.length === 1 && nums[0] === 0) ||
-        (nums.length === 2 && nums[0] === 733 && nums[1] === 100000)
-  const subscript = components.map(c => parseInt(c, 10)).filter(n => !isNaN(n))
-
-  const plus = isHeading ? subscript : subscript.slice(0, 1)
-  const para = isHeading ? [] : subscript.slice(1, 2)
+  const plus = []
+  for (const c of components) {
+    const n = parseInt(c, 10)
+    if (isNaN(n)) continue
+    plus.push(n * (c.startsWith('0') ? -1 : 1))
+  }
 
   const parsed = { nums }
   if (plus.length) parsed.plus = plus
-  if (para.length) parsed.para = para
 
   return parsed
 }
 
-export const toString = ({ nums, plus, para }) => {
+export const toString = ({ nums, plus }) => {
   const str = [
     nums.length === 1 ? 'NP ' : '',
     nums.map(n => n.toString()).join('.'),
-    (plus || []).map(n => `[${n}]`).join(''),
-    (para || []).map(n => `[0${n}]`).join('')
+    (plus || []).map(n => `[${n < 0 ? '0' : ''}${Math.abs(n)}]`).join('')
   ].join('')
 
   return str
@@ -60,32 +58,27 @@ export const toString = ({ nums, plus, para }) => {
     .replace(/827.30\[([0-9]+)]/, 'Ep $1')
 }
 
-const compareComponent = (a, b, lengthFactor) => {
-  if (a.length > 0 && a[0] === 0) {
-    return -1
-  } else if (b.length > 0 && b[0] === 0) {
-    return 1
-  }
-
-  for (let nn = 0, nl = Math.min(a.length, b.length); nn < nl; nn++) {
-    const an = a[nn]
-    const bn = b[nn]
+const compareComponent = (a, b) => {
+  for (let nn = 0, nl = Math.max(a.length, b.length); nn < nl; nn++) {
+    let an = nn < a.length ? a[nn] : 0
+    let bn = nn < b.length ? b[nn] : 0
     if (an === bn) {
       continue
     }
+    if ((an < 0 && bn < 0) || (an > 0 && bn > 0)) {
+      an = Math.abs(an)
+      bn = Math.abs(bn)
+    }
     return an - bn
   }
-
-  return lengthFactor * (a.length - b.length)
+  return 0
 }
 
 export const compare = (a, b) => {
   a = np2p(a)
   b = np2p(b)
-
-  return compareComponent(a.nums, b.nums, 1) ||
-    compareComponent(a.plus || [], b.plus || [], -1) ||
-    compareComponent(a.para || [], b.para || [], 1)
+  return compareComponent(a.nums, b.nums) ||
+    compareComponent(a.plus || [], b.plus || [])
 }
 
 export const within = ([startIncl, endIncl], v) => {
