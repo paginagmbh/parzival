2f1e5b33a4bd063c875100e0f0a7bb850ffe008b
Kai Weber <kai.weber@pagina.gmbh>
2020-09-09 13:42:57 +0200

    Updated vulnerable dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,15 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
-  rules: {
-    'eqeqeq': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  rules: {
+    'eqeqeq': 'off',
+    'no-unused-vars': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
